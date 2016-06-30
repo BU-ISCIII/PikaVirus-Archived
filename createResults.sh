d997252591ceb8cd4046e0d007048a52ac7bdcd5
Andrea <andrea.rubiop88@hotmail.com>
2016-06-30 17:39:14 +0200

    Add files via upload

---
+++ createResults.sh
@@ -0,0 +1,32 @@
+
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <style></style>
+  </head>
+  <body>
+    <table class="flatTable">
+      <tr class="titleTr">
+	<td class="titleTd">TABLE TITLE</td>
+	<td colspan="4"></td>
+	<td class="plusTd button"></td>
+      </tr>
+      <tr class="headingTr">
+	<td>Subject Title</td>
+	<td>Query Seq-id</td>
+	<td>Percentage of identical matches</td>
+	<td>Alignment length</td>
+	<td>Number of mismatches</td>
+	<td>Number of gap openings</td>
+	<td>Start of alignment in query</td>
+	<td>End of alignment in query</td>
+	<td>Start of alignment in subject</td>
+	<td>End of alignment in subject</td>
+	<td>Expect value</td>
+	<td>Bit score</td>
+	<td></td>
+      </tr>
+    </table>
+    </body>
+    </html>
