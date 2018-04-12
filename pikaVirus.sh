ba873b7139030f35ac0367e39cc02e825572b57d
Miguel <migueljuliamolina@gmail.com>
2018-04-12 09:29:17 +0200

    sge parallelization implemented

--- pikaVirus.sh
+++ pikaVirus.sh
@@ -112,15 +112,27 @@ awk 'a[$0]++{exit 1}' ${analysisDir}samples_id.txt ||
 echo "Please rename your files or modify the regular expression in ${PIKAVIRUSDIR}/samplesID_gen.sh to make it work with your samples" &&
 exit 1)
 samplesIdFile="${analysisDir}/samples_id.txt"
+sample_count="$( wc -l ${analysisDir}/samples_id.txt | cut -f1 -d' ')"
 
 # QUALITY CONTROL
 if [ $cluster == "yes" ]
 then
-	# TO-DO
-	# in=$(awk "NR==$SGE_TASK_ID" $samplesIdFile)
-	# bash ${PIKAVIRUSDIR}/preprocessing.sh -s $in
-	echo "TO-DO: This option si not supported yet"
-	exit 1
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N preprocessing_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_preprocessing.txt" ]
+	then
+		echo "preprocessing_\$SGE_TASK_ID" > ${analysisDir}/jid_preprocessing.txt
+	else
+		echo ",preprocessing_\$SGE_TASK_ID" > ${analysisDir}/jid_preprocessing.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/preprocessing.sh -s \$INPUT
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
 else
 	cat ${analysisDir}/samples_id.txt | while read in
 	do
@@ -135,16 +147,106 @@ perl ${PIKAVIRUSDIR}/html/quality/createHTML.pl
 # MAPPING
 if [ $cluster == "yes" ]
 then
-	# TO-DO
-	# in=$(awk "NR==$SGE_TASK_ID" $samplesIdFile)
-	# bash ${PIKAVIRUSDIR}/host_removal.sh $in
-	# bash ${PIKAVIRUSDIR}/mapper_bac.sh $in
-	# bash ${PIKAVIRUSDIR}/mapper_virus.sh $in
-	# bash ${PIKAVIRUSDIR}/mapper_fungi.sh $in
-	# bash ${PIKAVIRUSDIR}/mapper_parasite.sh $in
-	# bash ${PIKAVIRUSDIR}/mapper_unknown.sh $in
-	echo "TO-DO: This option si not supported yet"
-	exit 1
+	while [ (! -f ${analysisDir}/jid_preprocessing.txt) || $( cat ${analysisDir}/jid_preprocessing.txt | sed -e 's/,/\n/g' | wc -l ) != $sample_count ]
+	do
+		sleep 1
+	done
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N host_removal_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "host_removal_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",host_removal_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/host_removal.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N mapper_bac_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "mapper_bac_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",mapper_bac_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/mapper_bac.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N mapper_virus_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "mapper_virus_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",mapper_virus_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/mapper_virus.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N mapper_fungi_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "mapper_fungi_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",mapper_fungi_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/mapper_fungi.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N mapper_parasite_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "mapper_parasite_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",mapper_parasite_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/mapper_parasite.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N mapper_unknown_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_mapping.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_mapping.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	bash ${PIKAVIRUSDIR}/mapper_unknown.sh \$INPUT
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_preprocessing.txt )  bash tmp.sh
+	rm tmp.sh
 else
 	cat ${analysisDir}/samples_id.txt | while read in
 	do
@@ -160,11 +262,112 @@ fi
 # ASSEMBLY
 if [ $cluster == "yes" ]
 then
-	# TO-DO
-	# in=$(awk "NR==$SGE_TASK_ID" $samplesIdFile)
-	# bash ${PIKAVIRUSDIR}/assembly.sh $in
-	echo "TO-DO: This option si not supported yet"
-	exit 1
+	while [ (! -f ${analysisDir}/jid_mapping.txt) || $( cat ${analysisDir}/jid_mapping.txt | sed -e 's/,/\n/g' | wc -l ) != $sample_count*6 ]
+	do
+		sleep 1
+	done
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_bac_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/05-bacteria/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_virus_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/06-virus/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_fungi_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/07-fungi/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_protozoa_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/08-protozoa/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_invertebrate_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/09-invertebrate/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N assembly_unknown_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_assembly.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_assembly.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	mappedDir="${analysisDir}/10-unknown/\${INPUT}/reads/"
+	bash ${PIKAVIRUSDIR}/assembly.sh \$mappedDir
+	EndOfFile
+	$cluster_prefix -hold_jib $( cat ${analysisDir}/jid_mapping.txt ) bash tmp.sh
+	rm tmp.sh
 else
 	cat ${analysisDir}/samples_id.txt | while read in
 	do
@@ -186,11 +389,112 @@ fi
 # BLAST
 if [ $cluster == "yes" ]
 then
-	# TO-DO
-	# in=$(awk "NR==$SGE_TASK_ID" $samplesIdFile)
-	# bash ${PIKAVIRUSDIR}/blast.sh $in
-	echo "TO-DO: This option si not supported yet"
-	exit 1
+	while [ (! -f ${analysisDir}/jid_assembly.txt) || $( cat ${analysisDir}/jid_assembly.txt | sed -e 's/,/\n/g' | wc -l ) != $sample_count*6 ]
+	do
+		sleep 1
+	done
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_bac_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/05-bacteria/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir $bacDB
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_virus_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/06-virus/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir $bacDB
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_fungi_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/07-fungi/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir $fungiDB
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_protozoa_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/08-protozoa/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir $protozoaDB
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_invertebrate_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/09-invertebrate/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir $invertebrateDB
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N blast_unknown_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_blast.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_blast.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/10-unknown/\${INPUT}/"
+	bash ${PIKAVIRUSDIR}/blast.sh \$sampleDir
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
 else
 	cat ${analysisDir}/samples_id.txt | while read in
 	do
@@ -212,11 +516,105 @@ fi
 # COVERAGE
 if [ $cluster == "yes" ]
 then
-	# TO-DO
-	# in=$(awk "NR==$SGE_TASK_ID" $samplesIdFile)
-	# bash ${PIKAVIRUSDIR}/coverage.sh $in
-	echo "TO-DO: This option si not supported yet"
-	exit 1
+	while [ (! -f ${analysisDir}/jid_blast.txt) || $( cat ${analysisDir}/jid_blast.txt | sed -e 's/,/\n/g' | wc -l ) != $sample_count*6 ]
+	do
+		sleep 1
+	done
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N coverage_bac_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_coverage.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/05-bacteria/\${INPUT}/"
+	sampleName=\$(basename \$sampleDir)
+	bash ${PIKAVIRUSDIR}/coverage.sh \$sampleDir $bacDB
+	Rscript --vanilla "${PIKAVIRUSDIR}/graphs_coverage.R" "\${sampleDir}/coverage/" \${sampleName}
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N coverage_virus_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_coverage.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/06-virus/\${INPUT}/"
+	sampleName=\$(basename \$sampleDir)
+	bash ${PIKAVIRUSDIR}/coverage.sh \$sampleDir $bacDB
+	Rscript --vanilla "${PIKAVIRUSDIR}/graphs_coverage.R" "\${sampleDir}/coverage/" \${sampleName}
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N coverage_fungi_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_coverage.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/07-fungi/\${INPUT}/"
+	sampleName=\$(basename \$sampleDir)
+	bash ${PIKAVIRUSDIR}/coverage.sh \$sampleDir $fungiDB
+	Rscript --vanilla "${PIKAVIRUSDIR}/graphs_coverage.R" "\${sampleDir}/coverage/" \${sampleName}
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N coverage_protozoa_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_coverage.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/08-protozoa/\${INPUT}/"
+	sampleName=\$(basename \$sampleDir)
+	bash ${PIKAVIRUSDIR}/coverage.sh \$sampleDir $protozoaDB
+	Rscript --vanilla "${PIKAVIRUSDIR}/graphs_coverage.R" "\${sampleDir}/coverage/" \${sampleName}
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
+	cat > tmp.sh <<- EndOfFile
+	#!/bin/sh
+	#$ -N coverage_invertebrate_\$SGE_TASK_ID
+	#$ -t 1-$sample_count
+	if [ ! -f "${analysisDir}/jid_coverage.txt" ]
+	then
+		echo "mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	else
+		echo ",mapper_unknown_\$SGE_TASK_ID" > ${analysisDir}/jid_coverage.txt
+	fi
+	INPUTFILE=${analysisDir}/samples_id.txt
+	INPUT=\$(awk "NR==\$SGE_TASK_ID" \$INPUTFILE)
+	sampleDir="${analysisDir}/09-invertebrate/\${INPUT}/"
+	sampleName=\$(basename \$sampleDir)
+	bash ${PIKAVIRUSDIR}/coverage.sh \$sampleDir $invertebrateDB
+	Rscript --vanilla "${PIKAVIRUSDIR}/graphs_coverage.R" "\${sampleDir}/coverage/" \${sampleName}
+	EndOfFile
+	$cluster_prefix bash tmp.sh
+	rm tmp.sh
 else
 	cat ${analysisDir}/samples_id.txt | while read in
 	do
@@ -244,5 +642,12 @@ else
 fi
 
 # RESULTS
+if [ $cluster == "yes" ]
+then
+	while [ (! -f ${analysisDir}/jid_coverage.txt) || $( cat ${analysisDir}/jid_coverage.txt | sed -e 's/,/\n/g' | wc -l ) != $sample_count*5 ]
+	do
+		sleep 1
+	done
+fi
 bash generate_results.sh
 
