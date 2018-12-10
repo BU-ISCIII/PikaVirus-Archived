b992775398f1d6be50f07e1d1ab7b7880c316220
Miguel <migueljuliamolina@gmail.com>
2018-12-10 12:50:21 +0100

    updated image links

--- html/persample/createSamplesHtml.sh
+++ html/persample/createSamplesHtml.sh
@@ -16,10 +16,10 @@ cat ${PIKAVIRUSDIR}/html/header.html > $result_page
 echo "
 <nav class=\"navbar navbar-inverse\" data-spy=\"affix\" data-offset-top=\"137\">
   <ul class=\"nav navbar-nav\">
-    <li><a href=\"summary.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/clipboard.png' img> Summary</a></li>
-    <li class=\"active\"><a href=\"samples.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/test-tube.png' img> Results per sample</a></li>
-    <li><a href=\"quality.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/dna-structure.png' img> Quality Analysis</a></li>
-    <li><a href=\"info.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/book.png' img> What is this?</a></li>
+    <li><a href=\"summary.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/clipboard.png' img> Summary</a></li>
+    <li class=\"active\"><a href=\"samples.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/test-tube.png' img> Results per sample</a></li>
+    <li><a href=\"quality.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/dna-structure.png' img> Quality Analysis</a></li>
+    <li><a href=\"info.html\"><img class=\"icon-bar\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/book.png' img> What is this?</a></li>
   </ul>
 </nav>
 
@@ -34,9 +34,9 @@ do
 	echo "
 	<li id=\"$in\" class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">$in</a>
         <ul class=\"dropdown-menu\">
-          <li id=\"${in}-bac\" data-target=\"${in}-bac\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/bacteria.png' img>  Bacteria</a></li>
-          <li id=\"${in}-vir\" data-target=\"${in}-vir\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/virus1.png' img>  Virus</a></li>
-          <li id=\"${in}-fun\" data-target=\"${in}-fun\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/feature/nextflow/html/img/fungi.png' img>  Fungi</a></li>
+          <li id=\"${in}-bac\" data-target=\"${in}-bac\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/bacteria.png' img>  Bacteria</a></li>
+          <li id=\"${in}-vir\" data-target=\"${in}-vir\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/virus1.png' img>  Virus</a></li>
+          <li id=\"${in}-fun\" data-target=\"${in}-fun\"><a href=\"#\"><img class=\"icon\" src='https://raw.githubusercontent.com/BU-ISCIII/PikaVirus/develop/html/img/fungi.png' img>  Fungi</a></li>
         </ul>
       </li>
 	" >> $result_page
